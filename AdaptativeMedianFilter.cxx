e165513a0ade292d9a7b430e83186a447b42b427
Juan Fernando Rojas <egeda9@gmail.com>
2023-03-22 10:06:42 -0500

    Add files via upload

--- AdaptativeMedianFilter.cxx
+++ AdaptativeMedianFilter.cxx
@@ -1,27 +1,36 @@
-#include "itkMedianImageFilter.h"
+#include <iostream>
+#include <string>
 #include "itkImage.h"
 #include "itkImageFileReader.h"
-#include <itkShapedNeighborhoodIterator.h>
-#include <itkNeighborhood.h>
+#include "itkImageFileWriter.h"
+#include "itkNeighborhood.h"
+#include "itkConstNeighborhoodIterator.h"
+#include "itkNeighborhoodAlgorithm.h"
+#include "itkStatisticsImageFilter.h"
+#include "itkNeighborhoodAlgorithm.h"
 
 int main(int argc, char * argv[])
 {
     if (argc != 4)
     {
         std::cerr << "Usage: " << std::endl;
-        std::cerr << argv[0] << " <InputImageFile> <OutputImageFile> <radius>" << std::endl;
+        std::cerr << argv[0] << " <InputImageFile> <OutputImageFile> <maxRadiusValue>" << std::endl;
         return EXIT_FAILURE;
     }
 
+    using PixelType = unsigned char;
     constexpr unsigned int Dimension = 3;
 
     const char * inputFileName = argv[1];
     const char * outputFileName = argv[2];
-    const int    radiusValue = std::stoi(argv[3]);
+    const int    maxRadiusValue = std::stoi(argv[3]);
+    int          minRadiusValue = 1;
 
-    using PixelType = unsigned char;
     using ImageType = itk::Image<PixelType, Dimension>;
     using ReaderType = itk::ImageFileReader<ImageType>;
+    using WriterType = itk::ImageFileWriter<ImageType>;
+
+    using NeighborhoodIteratorType = itk::ConstNeighborhoodIterator<ImageType>;
 
     auto reader = ReaderType::New();
     reader->SetFileName(inputFileName);
@@ -36,42 +45,83 @@ int main(int argc, char * argv[])
         return EXIT_FAILURE;
     }
 
-    using NeighborhoodIteratorType = itk::ShapedNeighborhoodIterator<ImageType>;
+    ImageType::Pointer outputImage = ImageType::New();
+    outputImage->SetRegions(reader->GetOutput()->GetLargestPossibleRegion());
+    outputImage->CopyInformation(reader->GetOutput());
+    outputImage->Allocate();
 
     NeighborhoodIteratorType::RadiusType radius;
-    radius.Fill(radiusValue);
-    NeighborhoodIteratorType it(radius, reader->GetOutput(), reader->GetOutput()->GetLargestPossibleRegion());
-    std::cout << "By default there are " << it.GetActiveIndexListSize() << " active indices." << std::endl;
+    radius.Fill(minRadiusValue);
 
-    NeighborhoodIteratorType::OffsetType top = { { 0, -1 } };
-    it.ActivateOffset(top);
-    NeighborhoodIteratorType::OffsetType bottom = { { 0, 1 } };
-    it.ActivateOffset(bottom);
+    using IteratorType = itk::ImageRegionIterator<ImageType>;
+    IteratorType out(outputImage, outputImage->GetRequestedRegion());
 
-    std::cout << "Now there are " << it.GetActiveIndexListSize() << " active indices." << std::endl;
+    NeighborhoodIteratorType iterator(radius, reader->GetOutput(),reader->GetOutput()->GetLargestPossibleRegion());
 
-    NeighborhoodIteratorType::IndexListType                 indexList = it.GetActiveIndexList();
-    NeighborhoodIteratorType::IndexListType::const_iterator listIterator = indexList.begin();
-
-    // Note that ZeroFluxNeumannBoundaryCondition is used by default so even
-    // pixels outside of the image will have valid values (equivalent to
-    // their neighbors just inside the image)
-    for (it.GoToBegin(); !it.IsAtEnd(); ++it)
+    for (iterator.GoToBegin(), out.GoToBegin(); !iterator.IsAtEnd(); ++iterator, ++out)
     {
-        std::cout << "New position: " << std::endl;
-        NeighborhoodIteratorType::ConstIterator ci = it.Begin();
-
-        while (!ci.IsAtEnd())
+        radius[0] = minRadiusValue;
+        radius[1] = minRadiusValue;
+        radius[2] = minRadiusValue;
+        iterator.SetRadius(radius);
+        bool repeatProcess = true;
+        do
         {
-            std::cout << "Centered at " << it.GetIndex() << std::endl;
-            std::cout << "Neighborhood index " << ci.GetNeighborhoodIndex() << " is offset " << ci.GetNeighborhoodOffset()
-                      << " and has value " << ci.Get() << " The real index is "
-                      << it.GetIndex() + ci.GetNeighborhoodOffset() << std::endl;
-            ++ci;
-        }
+            std::vector<PixelType> values;
+            for (unsigned int i = 0; i < iterator.Size(); i++) {
+                if (i != iterator.Size() / 2) {
+                    values.push_back(iterator.GetPixel(i));
+                }
+            }
+            std::sort(values.begin(), values.end());
+            PixelType median = values[values.size() / 2];
+            PixelType maxValue = *std::max_element(values.begin(), values.end());
+            PixelType minValue = *std::min_element(values.begin(), values.end());
+            PixelType centerValue = iterator.GetPixel(iterator.Size() / 2);
+            if(minValue < median && median < maxValue)
+            {
+                repeatProcess = false;
+                if(minValue < centerValue && centerValue < maxValue)
+                {
+                    out.Set(centerValue);
+                }
+                else
+                {
+                    out.Set(median);
+                }
+            }
+            else
+            {
+                radius[0]++;
+                radius[1]++;
+                radius[2]++;
+                iterator.SetRadius(radius);
+            }
+            if(iterator.GetRadius()[0] <= maxRadiusValue)
+            {
+                continue;
+            }
+            else
+            {
+                repeatProcess = false;
+                out.Set(median);
+            }
+        } while(repeatProcess);
     }
-
-    std::cout << std::endl;
+    using WriterType = itk::ImageFileWriter<ImageType>;
+    auto writer = WriterType::New();
+    writer->SetFileName(outputFileName);
+    writer->SetInput(outputImage);
+    try    {
+        writer->Update();
+    }
+    catch (const itk::ExceptionObject & err)
+    {
+        std::cerr << "ExceptionObject caught !" << std::endl;
+        std::cerr << err << std::endl;
+        return EXIT_FAILURE;
+    }
 
     return EXIT_SUCCESS;
 }
+
